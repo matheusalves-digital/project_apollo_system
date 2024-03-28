90f3731ed81fedbf15bcc054b78e6401d4ef6cdf
Matheus Fontinele Alves Vieira <matheusfont.123@gmail.com>
2024-03-27 23:11:51 -0300

    feat: add update triage to colaborator user

--- triage/urls.py
+++ triage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TriageCreateView, TriageListView, TriageDetailView
+from .views import TriageCreateView, TriageListView, TriageDetailView, TriageUpdateView
 
 urlpatterns = [
     path('create_triage/', TriageCreateView.as_view(), name='triage-create'),
     path('list_triage/', TriageListView.as_view(), name='triage-list'),
     path('<str:pk>/', TriageDetailView.as_view(), name='triage-detail'),
+    path('<str:pk>/update/', TriageUpdateView.as_view(), name='triage-update'),
 ]
